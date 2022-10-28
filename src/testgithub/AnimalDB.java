b6501e3b5750aec300bedd9af024b6bdbcc63d67
Derick Dela Cruz <derickbdc8@gmail.com>
2022-10-28 14:48:25 +1300

    added primary key, foreign key, embedded database not working currently

--- src/testgithub/AnimalDB.java
+++ src/testgithub/AnimalDB.java
@@ -15,98 +15,100 @@ import java.sql.Statement;
  * @author DDC
  */
 public class AnimalDB {
+
     private final DBManager dbManager;
     private final Connection conn;
     private Statement statement;
     private Animal animal;
     private String key;
-    
-    public AnimalDB(){
+
+    public AnimalDB() {
         dbManager = new DBManager();
         conn = dbManager.getConnection();
     }
-    
-    public void createAnimalDB(){
+
+    public void createAnimalDB() {
         //use the conn, initialize database by creating Animal Table
-        try{
+        try {
             this.statement = this.conn.createStatement();
             this.checkExisting("ANIMAL");
-            this.statement.addBatch("CREATE TABLE ANIMAL (ANIMALID VARCHAR(50), NAME VARCHAR(50), TYPE VARCHAR(20), LEVEL INT, "
-                    + "XP INT, HAPPINESS INT, HEALTH INT, HUNGER INT, SLEEP INT, XPCAP INT, HAPPYCAP INT, HEALTHCAP INT,"
-                    + "HUNGERCAP INT, SLEEPCAP INT)");
-            
+            this.statement.addBatch("CREATE TABLE ANIMAL (NAME VARCHAR(50) NOT NULL, TYPE VARCHAR(20) NOT NULL, LEVEL INT NOT NULL, "
+                    + "XP INT, HAPPINESS INT NOT NULL, HEALTH INT NOT NULL, HUNGER INT NOT NULL, SLEEP INT NOT NULL, XPCAP INT, HAPPYCAP INT NOT NULL, HEALTHCAP INT NOT NULL,"
+                    + "HUNGERCAP INT NOT NULL, SLEEPCAP INT NOT NULL, MONEY FLOAT, CONSTRAINT animal_pk PRIMARY KEY (NAME, TYPE))");
             this.statement.executeBatch();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Error: Unable to create table Animal");
         }
     }
-    
-    public void insertAnimal(){
-        try{
+
+    public void insertAnimal() {
+        try {
             this.statement = this.conn.createStatement();
-            key = animal.getAnimalName()+"_"+animal.getAnimalType();
-            this.statement.addBatch("INSERT INTO ANIMAL ("+key+", "+animal.getAnimalName()+", "+ animal.getAnimalType() +", "+animal.lvl.getLevel()+", "
-                    +animal.lvl.getXp()+", "+animal.getHappiness()+", "+animal.getHealth()+", "+animal.getHunger()+", "+animal.getSleep()+", "
-                    +animal.lvl.getLevelXpCap()+", "+animal.getHappinessCap()+", "+animal.getHealthCap()+", "+animal.getHungerCap()+","+animal.getSleepCap()+")");
-            
+            this.statement.addBatch("INSERT INTO ANIMAL ('" + animal.getAnimalName() + "', '" + animal.getAnimalType() + "', " + animal.lvl.getLevel() + ", "
+                    + animal.lvl.getXp() + ", " + animal.getHappiness() + ", " + animal.getHealth() + ", " + animal.getHunger() + ", " + animal.getSleep() + ", "
+                    + animal.lvl.getLevelXpCap() + ", " + animal.getHappinessCap() + ", " + animal.getHealthCap() + ", " + animal.getHungerCap() + "," + animal.getSleepCap() + "," 
+                    + animal.store.money.getAmount()+")");
+
             this.statement.executeBatch();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Error: Unable to insert into table Animal");
         }
     }
-    
-    public void createInventoryBD(){
+
+    public void createInventoryBD() {
         //use the conn, initialize database by creating FOOD Table
-        try{
+        try {
             this.statement = this.conn.createStatement();
             this.checkExisting("INVENTORY");
-            this.statement.addBatch("CREATE TABLE INVENTORY (INVENTORYID INT, TYPE VARCHAR(50),  ITEM VARCHAR(50))");
-            this.statement.addBatch("INSERT INTO INVENTORY VALUES(1, 'foods', 'basic'), (2, 'foods', 'deluxe'), (3, 'foods', 'premium')"
-                    + ", (4, 'toys', 'ball'), (5, 'toys', 'yarn'), (6, 'toys', 'chew toy'), (7, 'toys', 'mice toy'), (8, 'toys', 'frisbee'),"
-                    + "(9, 'toys', 'laser pointer'), (10, 'beds', 'double'), (11, 'beds', 'queen'), (12, 'beds', 'king')");
+            this.statement.addBatch("CREATE TABLE INVENTORY (INVENTORYID INT NOT NULL PRIMARY KEY, TYPE VARCHAR(50),  ITEM VARCHAR(50), PRICE FLOAT)");
+            this.statement.addBatch("INSERT INTO INVENTORY VALUES(1, 'foods', 'basic', 10.5), (2, 'foods', 'deluxe', 14), (3, 'foods', 'premium', 20)"
+                    + ", (4, 'toys', 'ball', 7.5), (5, 'toys', 'yarn', 8), (6, 'toys', 'chew toy', 11), (7, 'toys', 'mice toy', 10.5), (8, 'toys', 'frisbee', 20),"
+                    + "(9, 'toys', 'laser pointer', 25), (10, 'beds', 'double', 11.5), (11, 'beds', 'queen', 23), (12, 'beds', 'king', 30)");
             this.statement.executeBatch();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Error: Unable to create table Inventory");
         }
     }
-    
-    public void insertInventory(String ID, String item){
-        try{
+
+    public void insertInventory(String ID, String item) {
+        try {
             this.statement = this.conn.createStatement();
-            this.statement.addBatch("INSERT INTO INVENTORY ("+ID+", "+ item+")");
-            
+            this.statement.addBatch("INSERT INTO INVENTORY (" + ID + ", " + item + ")");
+
             this.statement.executeBatch();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Error: Unable to insert in Inventory table");
         }
     }
-    
-    public void createAnimalInvenDB(){
-        try{
+
+    public void createAnimalInvenDB() {
+        try {
             this.statement = this.conn.createStatement();
             this.checkExisting("ANIMAL_INVENTORY");
-            this.statement.addBatch("CREATE TABLE ANIMAL_INVENTORY (INVENTORYID INT, ANIMALID VARCHAR(50))");
-            
+            this.statement.addBatch("CREATE TABLE ANIMAL_INVENTORY (INVENTORYID INT, NAME VARCHAR(50), "
+                    + "TYPE VARCHAR(50), CONSTRAINT inven_fk FOREIGN KEY (INVENTORYID) REFERENCES INVENTORY(INVENTORYID), "
+                    + "CONSTRAINT name_fk FOREIGN KEY (NAME) REFERENCES ANIMAL(NAME), "
+                    + "CONSTRAINT type_fk FOREIGN KEY (TYPE) REFERENCES ANIMAL(TYPE))");
             this.statement.executeBatch();
-        } catch (SQLException e){
+        } catch (SQLException e) {
+            System.out.println(e);
             System.out.println("Error: Unable to create table Animal Inventory");
         }
     }
-    
-    public void insertAnimalInvenDB(int inventoryID){
-        try{
+
+    public void insertAnimalInvenDB(int inventoryID) {
+        try {
             this.statement = this.conn.createStatement();
-            this.statement.addBatch("INSERT INTO ANIMAL_INVENTORY ("+inventoryID+", "+key+")");
-            
+            this.statement.addBatch("INSERT INTO ANIMAL_INVENTORY (" + inventoryID + ", '" + animal.getAnimalName() + "', '"+animal.getAnimalType()+"')");
+
             this.statement.executeBatch();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Error: Unable to insert in table Animal Inventory");
         }
     }
-    
-    
-    public void checkExisting(String name){
-        try{
+
+    public void checkExisting(String name) {
+        try {
             DatabaseMetaData MetaData = this.conn.getMetaData();
             String[] types = {"TABLE"};
             statement = this.conn.createStatement();
@@ -114,19 +116,19 @@ public class AnimalDB {
             while (result.next()) {
                 String tableTitle = result.getString("TABLE_NAME");
                 //System.out.println("Table '"+tableTitle+"' exists.");
-                if (tableTitle.equalsIgnoreCase(name)){
-                    statement.executeUpdate("Drop table "+name);
+                if (tableTitle.equalsIgnoreCase(name)) {
+                    statement.executeUpdate("Drop table " + name);
                     //System.out.println("The table '"+name+"' has been deleted.");
                     break;
                 }
             }
             result.close();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Error");
         }
     }
-    
-    public void disconnect(){
+
+    public void disconnect() {
         this.dbManager.closeConnections();
     }
 }
--- src/testgithub/DBManager.java
+++ src/testgithub/DBManager.java
@@ -18,7 +18,7 @@ public final class DBManager {
 
     private static final String USER_NAME = "pdc"; //your DB username
     private static final String PASSWORD = "pdc"; //your DB password
-    private static final String URL = "jdbc:derby://localhost:1527/AnimalDB; create=true";  //url of the DB host
+    private static final String URL = "jdbc:derby:AnimalDB_Ebd; create=true";  //url of the DB host
 
     Connection conn;
 
